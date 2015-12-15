a5a0961d60c4029a4e79e96b7c3606bcbb6f2e87
aaronmarkle <aaronkmarkle@gmail.com>
2015-12-15 11:34:09 -0800

    roster.html displays full names of the players taken from api call

--- roster.js
+++ roster.js
@@ -1,8 +1,16 @@
 var getTeamProfile = new XMLHttpRequest();
 getTeamProfile.onload = function() {
-  //var teamId = teamId
   response = JSON.parse(getTeamProfile.responseText);
-  console.log(response);
+  listPlayers();
 }
 getTeamProfile.open('GET', 'http://127.0.0.1:8080/teamProfile', true);
 getTeamProfile.send();
+
+function listPlayers() {
+  for (var i=0; i<response.players.length; i++) {
+    var newNode = document.createTextNode(response.players[i].full_name);
+    var newEle = document.createElement('p');
+    newEle.appendChild(newNode);
+    document.getElementById('roster').appendChild(newEle);
+  }
+}
