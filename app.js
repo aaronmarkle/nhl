d5a2cb46df5c2886634e75822f8068ee97b037a1
aaronmarkle <aaronkmarkle@gmail.com>
2015-12-14 16:24:19 -0800

    setup endpoint for standings data to be sent from server to front end #1

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var rankings = require('./rankings.js');
 var standings = require('./standings.js');
 
-app.use('/standings', standings);
+//app.use('/rankings', rankings);
+
+//app.use('/standings', standings);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -13,12 +16,21 @@ app.get('/default.js', function(req, res) {
   res.sendFile(__dirname + '/default.js');
 });
 
-/*app.get('/rankings', function(req, res) {
-  var rankings;
+app.get('/default.css', function(req, res) {
+  res.sendFile(__dirname + '/default.css');
+})
+
+app.get('/rankings', function(req, res) {
   fs.readFile('rankingsSample.json', 'utf8', function(err, data) {
     res.send(data);
   });
-});*/
+});
+
+app.get('/standings', function(req, res) {
+  fs.readFile('standingsSample.json', 'utf8', function(err, data) {
+    res.send(data);
+  });
+});
 
 app.listen(8080);
 console.log('Server is live on port 8080.');
--- default.js
+++ default.js
@@ -1,6 +1,6 @@
-var xhr = new XMLHttpRequest();
-xhr.onload = function() {
-  response = JSON.parse(xhr.responseText);
+var getRankings = new XMLHttpRequest();
+getRankings.onload = function() {
+  response = JSON.parse(getRankings.responseText);
   //pacific
   document.getElementById('pd1').textContent = response.conferences[1].divisions[1].teams[0].name;
   document.getElementById('pd2').textContent = response.conferences[1].divisions[1].teams[1].name;
@@ -18,14 +18,14 @@ xhr.onload = function() {
   document.getElementById('cd6').textContent = response.conferences[1].divisions[0].teams[5].name;
   document.getElementById('cd7').textContent = response.conferences[1].divisions[0].teams[6].name;
   //atlantic
-  document.getElementById('ad1').textContent = response.conferences[0].divisions[0].teams[0].name;
-  document.getElementById('ad2').textContent = response.conferences[0].divisions[0].teams[1].name;
-  document.getElementById('ad3').textContent = response.conferences[0].divisions[0].teams[2].name;
-  document.getElementById('ad4').textContent = response.conferences[0].divisions[0].teams[3].name;
-  document.getElementById('ad5').textContent = response.conferences[0].divisions[0].teams[4].name;
-  document.getElementById('ad66').textContent = response.conferences[0].divisions[0].teams[5].name;
-  document.getElementById('ad7').textContent = response.conferences[0].divisions[0].teams[6].name;
-  document.getElementById('ad8').textContent = response.conferences[0].divisions[0].teams[7].name;
+  document.getElementById('atld1').textContent = response.conferences[0].divisions[0].teams[0].name;
+  document.getElementById('atld2').textContent = response.conferences[0].divisions[0].teams[1].name;
+  document.getElementById('atld3').textContent = response.conferences[0].divisions[0].teams[2].name;
+  document.getElementById('atld4').textContent = response.conferences[0].divisions[0].teams[3].name;
+  document.getElementById('atld5').textContent = response.conferences[0].divisions[0].teams[4].name;
+  document.getElementById('atld6').textContent = response.conferences[0].divisions[0].teams[5].name;
+  document.getElementById('atld7').textContent = response.conferences[0].divisions[0].teams[6].name;
+  document.getElementById('atld8').textContent = response.conferences[0].divisions[0].teams[7].name;
   //metropolitan
   document.getElementById('md1').textContent = response.conferences[0].divisions[1].teams[0].name;
   document.getElementById('md2').textContent = response.conferences[0].divisions[1].teams[1].name;
@@ -36,5 +36,13 @@ xhr.onload = function() {
   document.getElementById('md7').textContent = response.conferences[0].divisions[1].teams[6].name;
   document.getElementById('md8').textContent = response.conferences[0].divisions[0].teams[7].name;
 };
-xhr.open('GET', 'http://127.0.0.1:8080/standings', true);
-xhr.send();
+getRankings.open('GET', 'http://127.0.0.1:8080/rankings', true);
+getRankings.send();
+
+var getStandings = new XMLHttpRequest();
+getStandings.onload = function() {
+  response = JSON.parse(getStandings.responseText);
+  console.log(response);
+}
+getStandings.open('GET', 'http://127.0.0.1:8080/standings', true);
+getStandings.send();
